e88e3237d2e08ad294e036180d2f7a6b338024ef
Scot <scot.pfleghaar@gmail.com>
2018-01-14 13:37:03 -0600

    Framework Complete

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,5 +1,16 @@
 <?php
-    //Load Libraries
-    require_once 'libraries/core.php';
-    require_once 'libraries/controller.php';
-    require_once 'libraries/database.php';
+
+    // Load Congif file
+    require_once 'config/config.php';
+
+    // Load Libraries
+    // require_once 'libraries/core.php';
+    // require_once 'libraries/controller.php';
+    // require_once 'libraries/database.php';
+    // Rather than include each library like we do above, we can use the following function: 
+
+    // Autoload Core Libraries
+    // File names must tmatch class name 
+    spl_autoload_register(function($className){
+        require_once 'libraries/'.$className.'.php';
+    });
---
+++ app/config/config.php
@@ -0,0 +1,19 @@
+<?php
+//DB params
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PASS','root');
+define('DB_NAME','omvc');
+
+
+
+// App Root
+define('APPROOT', dirname(dirname(__FILE__)));
+
+//URL ROOT
+define('URLROOT','http://localhost:8888');
+
+//Site Name
+define('SITENAME', 'OpenMVC');
+
+
