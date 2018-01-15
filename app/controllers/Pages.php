d327391145d7983399d96869a7ff1c4edcf08941
Scot <scot.pfleghaar@gmail.com>
2018-01-15 12:08:51 -0600

    Update Comments

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -4,7 +4,7 @@ class Pages extends Controller {
         
     }
 
-
+// When logged in we redirect to the posts page
     public function index(){
         if(isLoggedIn()){
             redirect('posts');
@@ -13,9 +13,11 @@ class Pages extends Controller {
         $data = ['title'=>'Php Chat App Project',
     'description'=>'Built on a custom Php framework'];
 
+    // If the user is logged in home page is the pages/index
        $this->view('pages/index', $data);
     }
 
+    //When we are request the about page this is called
     public function about(){
         $data = ['title'=>'About Us', 'description'=>'App to share posts with other users'];
        $this->view('pages/about',$data);
